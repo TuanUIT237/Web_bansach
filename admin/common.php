e6fda1bc3320550e9f4062a21f4cb57ca2b40739
Đỗ Mạnh Tuấn <101437185+TuanUIT237@users.noreply.github.com>
2023-04-15 15:41:39 +0700

    Add files via upload

--- admin/common.php
+++ admin/common.php
@@ -1,1 +1,28 @@
+<?php
+    function addNew($table, $data){
+        global $con;
+        if(is_array($data)){
+            $field="";
+            $val="";
+            $i=0;
+            foreach($data as $key=>$value){
+                if($key!="addNew"){
+                    $i++;
+                    if($i==1){
+                        $field.=$key;
+                        $val.="'".$value."'";
+                    }else{
+                        $field.=",".$key;
+                        $val.=",'".$value."'";
+                    }
+                    
+                }
+            }
+        }
+    
+        $sqlInsert= "INSERT INTO $table($field)";
+        $sqlInsert.="VALUES($val)";
+        mysqli_query($con,$sqlInsert);
+    }
 
+?>
