852da5e3b5985f4cb3c1156bba2198205241e1ba
Đỗ Mạnh Tuấn <101437185+TuanUIT237@users.noreply.github.com>
2023-04-15 16:07:49 +0700

    Add files via upload

--- carbon/src/Carbon/Lang/bs_Latn.php
+++ carbon/src/Carbon/Lang/bs_Latn.php
@@ -8,8 +8,5 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-/*
- * Unknown default region, use the first alphabetically.
- */
-return require __DIR__.'/aa_DJ.php';
+return array_replace_recursive(require __DIR__.'/bs.php', [
+]);
